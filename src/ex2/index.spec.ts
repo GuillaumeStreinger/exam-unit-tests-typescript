cb98c11b3605951a9b50fc3e3500fe2a0b70db4d
GuillaumeStreinger <guillaume.streinger@yahoo.fr>
2024-07-25 15:00:57 +0200

    Exercice 2 : 1er test

--- src/ex2/index.spec.ts
+++ src/ex2/index.spec.ts
@@ -1,3 +1,21 @@
+import { submitOrder } from './index';
+import * as emailLib from './lib/email';
+
+jest.mock('./lib/email', () => ({
+  sendOrderEmail: jest.fn()
+}));
+
 describe("submitOrder", () => {
-  // écrire les tests ici
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Passe isSubmitted à True et appelle senOrderEmail si la commande n\'est pas encore livré', () => {
+    const order = { id: '1', isSubmitted: false };
+    const result = submitOrder(order);
+    expect(result.isSubmitted).toBe(true);
+    expect(emailLib.sendOrderEmail).toHaveBeenCalledWith('1');
+  });
+
 });
